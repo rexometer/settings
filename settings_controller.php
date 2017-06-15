f240b1cfa35cbd2f036442968c32f2d8a0037955
Simsala <saile@gmx.de>
2017-06-15 17:52:31 +0200

    initial commit

---
+++ settings_controller.php
@@ -0,0 +1,20 @@
+<?php
+/*
+    All Emoncms code is released under the GNU Affero General Public License.
+    See COPYRIGHT.txt and LICENSE.txt.
+
+    ---------------------------------------------------------------------
+    Emoncms - open source energy visualisation
+    Part of the OpenEnergyMonitor project:
+    http://openenergymonitor.org
+*/
+
+// no direct access
+defined('EMONCMS_EXEC') or die('Restricted access');
+
+function settings_controller()
+{
+    $result = view("Modules/settings/index.html", []);
+
+    return ['content' => $result];
+}
